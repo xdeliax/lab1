3f117b3353676b326814385087cafd94f1cf2f4a
Delia Ivascu <delia@delias-mac.local>
2024-04-22 14:34:45 -0700

    update

--- pipe.c
+++ pipe.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		if (pipe[pipes[i]] < 0)
 			{
 				perror("pipe");
-				return errno; // or exit/abort ??
+				exit(errno); // or exit/abort ??
 			}
 	}
 
@@ -30,25 +30,26 @@ int main(int argc, char *argv[])
 		if (pid != 0)
 		{
 			perror("fork");
-			return errno;
+			exit(errno);
 		}
 		else if (pid == 0) // for the child process
 		{
+			close()
 			if (i > 0) // set up read end of pipe (after the first command)
 			{
 				if (dup2(pipes[i - 1][0], STDIN_FILENO) == -1) // redirect standard input of current process to read from the read end of the last pipe
 				{
 					perror("dup2");
-                    return errno;
+                    exit(errno);
 				}
 			}
 
 			if (i < n - 1) // set up write end of pipe (before the last command)
 			{
-				if (dup2(pipes[i][1], STDIN_FILENO) == -1) // redirect standard output of current process to write to the write end of the pipe
+				if (dup2(pipes[i][1], STDOUT_FILENO) == -1) // redirect standard output of current process to write to the write end of the pipe
 				{
 					perror("dup2");
-                    return errno;
+                    exit(errno);
 				}
 			}
 
@@ -59,7 +60,7 @@ int main(int argc, char *argv[])
 
             execlp(argv[i + 1], argv[i + 1], NULL); // execute the executable
             perror("execlp");
-            return errno;
+            exit(errno);
 		}
 	}
 
@@ -75,15 +76,15 @@ int main(int argc, char *argv[])
         pid_t pid = wait(&status);
         if (pid == -1) {
             if (errno == ECHILD) {
-                return errno; // there are no more child processes
+                exit(errno); // there are no more child processes
             } else {
                 perror("wait");
-                return errno;
+                exit(errno);
             }
         } else {
             if (!WIFEXITED(status)) {
                 perror("Child process did not exit normally");
-    			return errno;
+    			exit(errno);
             }
         }
     }
